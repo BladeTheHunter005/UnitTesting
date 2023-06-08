453402cc00d3d9cf7b47243ce0778d1fc913023e
Brandon Bergan <132183661+BladeTheHunter005@users.noreply.github.com>
2023-06-07 17:55:59 -0700

    Update CILabTest.java

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -20,13 +20,37 @@ public class CILabTest {
 
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+        myString.setString("USA");
+        assertTrue(myString.detectCapitalUse(), "Failed for 'USA'");
     }
+
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("leetcode");
+        assertTrue(myString.detectCapitalUse(), "Failed for 'leetcode'");
     }
 
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("Google");
+        assertTrue(myString.detectCapitalUse(), "Failed for 'Google'");
+    }
 
+    @Test
+    public void detectCapitalUseTest4() {
+        myString.setString("OpenAI");
+        assertTrue(myString.detectCapitalUse(), "Failed for 'OpenAI'");
+    }
+
+    @Test
+    public void detectCapitalUseTest5() {
+        myString.setString("Hello");
+        assertFalse(myString.detectCapitalUse(), "Failed for 'Hello'");
+    }
+
+    @Test
+    public void detectCapitalUseTest6() {
+        myString.setString("WORLD");
+        assertFalse(myString.detectCapitalUse(), "Failed for 'WORLD'");
+    }
 }
